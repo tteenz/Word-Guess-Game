1528bf203ed1edfcfdb33c1e330709a82afb3be1
tteenz <trantiinaa@yahoo.com>
2018-10-06 23:26:50 -0700

    change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,81 @@
-var wins = 0;
-var losses = 0;
-var guesses = 10;
-var 
+var win = 0;
+var winCountElement = document.getElementById("win-cou");
 
 // List of space words
-    var spaceWords = ["space", "comet", "stars", "asteroid", "mars", "jupiter", "earth", "neptune", "mercury", "moon", "uranus", "eclipse", "universe", "rocket", "dust"];
-    console.log(spaceWords)
+this.spaceWords = ['space', 'comet', 'stars', 'asteroid', 'mars',
+    'jupiter', 'earth', 'neptune', 'mercury', 'moon',
+    'uranus', 'eclipse', 'universe', 'rocket', 'dust'];
+var game = {
+    guessed: [],
+    left: 10,
 
-    var word = spaceWords[Math.floor((Math.random() * spaceWords.length))];
-    var answerArray = [];
-    for (var i = 0; i < spaceWords.length; i++){
-        answerArray[i] = "_";
+    begin: function () {
+
+        this.complete = false;
+        this.word = spaceWords[Math.floor(Math.random() * spaceWords.length)];
+        this.$right = document.getElementById('right');
+        this.$wrong = document.getElementById('wrong');
+        this.$remain = document.getElementById('remain');
+
+        this.$right.innerHTML = '_'.repeat(this.word.length);
+
+    },
+    guess: function (correctLetter) {
+        if (this.left > 0 && this.complete != true) {
+            if (this.word.indexOf(correctLetter) > -1 || this.guessed.indexOf(correctLetter) > -1) {
+                this.right(correctLetter);
+            } else {
+                this.wrong(correctLetter);
+            }
+        }
+    },
+    right: function (correctLetter) {
+        for (var i = 0; i < this.word.length; i++) {
+            if (this.word[i] == correctLetter) {
+                var word = this.$right.innerHTML.split('');
+                word[i] = correctLetter;
+                this.$right.innerHTML = word.join('');
+            }
+        }
+
+        // When user inputs correct letters; alert win!
+        if (this.$right.innerHTML.indexOf('_') < 0) {
+            alert('You win! ' + this.word);
+            this.complete = true;
+        }
+    },
+    wrong: function (correctLetter) {
+        this.guessed.push(correctLetter);
+        this.$wrong.innerHTML += ' ' + correctLetter;
+        this.left--;
+        this.$remain.innerHTML = this.left;
+
+        // When user runs out of guesses; alert lost!
+        if (this.left <= 1) {
+            alert('You lose! ' + this.word);
+            this.complete = true;
+        }
     }
-    var remainingLetters = spaceWords.length;
+
+
+};
+
+// Allow user to input letters for guesses
+game.begin();
+document.onkeyup = function (event) {
+    var correctLetter = String.fromCharCode(event.keyCode).toLowerCase();
+    game.guess(correctLetter);
+};
+
+function Spaceman() {
+    tempString = wins + "";
+    for (var i = tempString.length; i < 45; i++) {
+        tempString += " ";
+    }
+    winCountElement.textContent = tempString;
+
+}
 
 
 
 
-// var answerArray = [];
-// for (var i = 0; i < word.length; i++) {
-//     answerArray[i] = "_";
-// }
-
-// var remainingLetters = word.length;
-
-
-
-// Word Guess
-// var wrongGuesses = 0;
-// $("button").on("click", function(){
-//     $(this).addClass("used");
-//     $(this).prop("disabled", "true");
-//     var matchFound = false;
-// }) 
-
-//
